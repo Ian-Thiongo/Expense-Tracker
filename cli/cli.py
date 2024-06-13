28641ca21a721e3abe1c5552ed3f170800432153
Ian <thiongoian6148@gmail.com>
2024-06-13 11:50:23 +0300

    implementing cli commands for adding expenses

--- cli/cli.py
+++ cli/cli.py
@@ -4,3 +4,13 @@ from Models.category import Category
 from orm.orm import ORM
 
 ORM('expenses.db') 
+
+@click.group()
+def cli():
+    """CLI for managing expenses."""
+    pass
+
+@cli.command()
+@click.argument('description')
+@click.argument('amount', type=float)
+@click.argument('category_id', type=int)
