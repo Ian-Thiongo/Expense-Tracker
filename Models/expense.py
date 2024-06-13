728b066ddd50d1c9e4f4db24524032513a6ca3b2
Ian <thiongoian6148@gmail.com>
2024-06-13 11:36:31 +0300

    Added ORM methods for Expense class operations.

--- Models/expense.py
+++ Models/expense.py
@@ -7,3 +7,19 @@ class Expense:
         self.description = description
         self.amount = amount
         self.category_id = category_id
+    
+    @classmethod
+    def create(cls, description, amount, category_id):
+        cls.orm.execute('INSERT INTO expenses (description, amount, category_id) VALUES (?, ?, ?)', (description, amount, category_id))
+
+    @classmethod
+    def delete(cls, expense_id):
+        cls.orm.execute('DELETE FROM expenses WHERE id = ?', (expense_id,))
+
+    @classmethod
+    def get_all(cls):
+        return cls.orm.fetchall('SELECT * FROM expenses')
+
+    @classmethod
+    def find(cls, expense_id):
+        return cls.orm.fetchone('SELECT * FROM expenses WHERE id = ?', (expense_id,))
