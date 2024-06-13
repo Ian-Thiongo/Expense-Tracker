7d21caff7f067425e9d722dd429789e0685db7b5
Ian <thiongoian6148@gmail.com>
2024-06-13 11:29:29 +0300

    Added ORM methods for Category class operations.

--- Models/category.py
+++ Models/category.py
@@ -5,3 +5,22 @@ class Category:
 
     def __init__(self, name):
         self.name = name
+
+    @classmethod
+    def create(cls, name):
+        cls.orm.execute('INSERT INTO categories (name) VALUES (?)', (name,))
+
+    @classmethod
+    def delete(cls, category_id):
+        cls.orm.execute('DELETE FROM categories WHERE id = ?', (category_id,))
+
+    @classmethod
+    def get_all(cls):
+        return cls.orm.fetchall('SELECT * FROM categories')
+
+    @classmethod
+    def find(cls, category_id):
+        return cls.orm.fetchone('SELECT * FROM categories WHERE id = ?', (category_id,))
+
+
+
